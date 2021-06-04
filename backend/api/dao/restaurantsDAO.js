c36e2ba0ff610fd0f47bb590960ed52fb8a9b1cc
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-06-04 00:51:22 -0400

    fixed naming error

--- backend/api/dao/restaurantsDAO.js
+++ backend/api/dao/restaurantsDAO.js
@@ -49,7 +49,7 @@ export default class RestaurantsDAO {
             return { restaurantsList, totalNumRestaurants }
         } catch (e) {
             console.error(
-                `Unable to convert cursor to array or problem counting documents, ${totalNumRestaurants}` // double check
+                `Unable to convert cursor to array or problem counting documents, ${e}` // double check
             )
             return { restaurantsList: [], totalNumRestaurants: 0 }
         }
