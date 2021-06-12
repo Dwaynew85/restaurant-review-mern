95d06ed7c53e5368dfd878876512b179fcf89e90
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-06-11 23:30:20 -0400

    refactored for readablility

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ import app from "./server.js"
 import mongodb from "mongodb"
 import dotenv from "dotenv"
 import RestaurantsDAO from "./dao/restaurantsDAO.js";
+
 dotenv.config()
+
 const MongoClient = mongodb.MongoClient
 
 const port = process.env.PORT || 8000
